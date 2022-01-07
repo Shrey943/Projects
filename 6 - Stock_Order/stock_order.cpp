2ac56f60e1b5836084c0f9c7d63f412c62a7decb
Shrey <shreychanderia@gmail.com>
2022-01-07 10:52:10 +0530

    list working ok, with priority

--- 6 - Stock_Order/stock_order.cpp
+++ 6 - Stock_Order/stock_order.cpp
@@ -1,7 +1,7 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <list>
 #include <fstream>
 using namespace std;
 
@@ -14,26 +14,56 @@ typedef struct order
 
 } order;
 
-int check(vector<order *> &vec, order *vec2, ofstream &executionFile)
+void display(list<order *> listRef)
 {
-    cout << std::fixed << std::setprecision(2); // to print upto 2 decimal
-
-    for (int i = 0; i < vec.size(); i++)
+    for (list<order *>::iterator iter = listRef.begin(); iter != listRef.end(); iter++)
     {
-        if (vec[i]->price == vec2->price && vec[i]->quantity == vec2->quantity)
-        {
-            if (vec2->orderType == 'B')
-            {
-                executionFile << "order " << vec2->orderId << " " << vec2->quantity << " shares purchased at price " << vec2->price << "\n";
-                executionFile << "order " << vec[i]->orderId << " " << vec[i]->quantity << " shares sold at price " << vec[i]->price << "\n";
-            }
-            else
-            {
-                executionFile << "order " << vec[i]->orderId << " " << vec[i]->quantity << " shares purchased at price " << vec[i]->price << "\n";
-                executionFile << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << vec2->price << "\n";
-            }
+        cout << "order " + (*iter)->orderId + " " << (*iter)->price << " " << (*iter)->quantity << endl;
+    }
+    // for (list<order *>::iterator iter = buy.begin(); iter != buy.end(); iter++)
+    // {
+    //     executionFile << "order " + (*iter)->orderId + " " << (*iter)->quantity << " shares unexecuted\n";
+    // }
+}
 
-            vec.erase(vec.begin() + i);
+int check(list<order *> &vec, order *vec2, ofstream &executionFile)
+{
+    typename list<order *>::iterator iter = vec.begin();
+
+    for (iter = vec.begin(); iter != vec.end(); iter++)
+    {
+        // Abhi sahi nai h, price greater mai chahiye
+        if (vec2->orderType == 'B' && (*iter)->price <= vec2->price && (*iter)->quantity == vec2->quantity)
+        {
+            // if (vec2->orderType == 'B')
+            // {
+            executionFile << "order " << vec2->orderId << " " << vec2->quantity << " shares purchased at price " << (*iter)->price << "\n";
+            executionFile << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares sold at price " << (*iter)->price << "\n";
+
+            cout << "\norder " << vec2->orderId << " " << vec2->quantity << " shares purchased at price " << (*iter)->price << "\n";
+            cout << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares sold at price " << (*iter)->price << "\n";
+
+            cout << "Before erase: " << endl;
+            display(vec);
+            vec.erase(iter);
+            cout << "After erase: " << endl;
+            display(vec);
+
+            return 1;
+        }
+        else if (vec2->orderType == 'S' && (*iter)->price >= vec2->price && (*iter)->quantity == vec2->quantity)
+        {
+            executionFile << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares purchased at price " << (*iter)->price << "\n";
+            executionFile << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << (*iter)->price << "\n";
+
+            cout << "\norder " << (*iter)->orderId << " " << (*iter)->quantity << " shares purchased at price " << (*iter)->price << "\n";
+            cout << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << (*iter)->price << "\n";
+
+            cout << "Before erase: " << endl;
+            display(vec);
+            vec.erase(iter);
+            cout << "After erase: " << endl;
+            display(vec);
 
             return 1;
         }
@@ -42,16 +72,42 @@ int check(vector<order *> &vec, order *vec2, ofstream &executionFile)
     return 0;
 }
 
+void insert(list<order *> &listRef, order *orderObj)
+{
+    typename list<order *>::iterator iter = listRef.begin();
+    if (!listRef.empty())
+    {
+        for (iter = listRef.begin(); iter != listRef.end(); iter++)
+        {
+            cout << "list: " << (*(iter))->orderId << endl;
+
+            if (orderObj->orderType == 'B' && orderObj->price > (*iter)->price)
+            {
+                break;
+            }
+            else if (orderObj->orderType == 'S' && orderObj->price < (*iter)->price)
+            {
+                break;
+            }
+        }
+    }
+    listRef.insert(iter, orderObj);
+    cout << "INserted: " << orderObj->orderId << endl;
+}
+
 int main()
 {
-    vector<order *> buy; // Stores pointer of order datatype
-    vector<order *> sell;
+    cout << std::fixed << std::setprecision(2); // to print upto 2 decimal
+
+    list<order *> buy; // Stores pointer of order datatype
+    list<order *> sell;
     float prevPrice;
 
     ifstream orderFile;
     ofstream executionFile("execution.txt");
+    executionFile << std::fixed << std::setprecision(2); // to print upto 2 decimal
 
-    orderFile.open("orders.txt");
+    orderFile.open("orders2.txt");
     orderFile >> prevPrice;
 
     while (orderFile.eof() == 0)
@@ -65,34 +121,36 @@ int main()
         {
             break;
         }
-        cout << orderObj->orderId << orderObj->orderType << temp << temp2 << orderObj->price << orderObj->quantity << endl;
+        // cout << orderObj->orderId << orderObj->orderType << temp << temp2 << orderObj->price << orderObj->quantity << endl;
 
         if (orderObj->orderType == 'B')
         {
-            
+            cout<<"BUY"<<endl;
             if (!check(sell, orderObj, executionFile))
             {
-
-                buy.push_back(orderObj);
+                insert(buy, orderObj);
+                // buy.push_back(orderObj);
             }
         }
         else
         {
+            cout<<"Sell"<<endl;
             if (!check(buy, orderObj, executionFile))
             {
+                insert(sell, orderObj);
 
-                sell.push_back(orderObj);
+                // sell.push_back(orderObj);
             }
         }
     }
 
-    for (int i = 0; i < sell.size(); i++)
+    for (list<order *>::iterator iter = sell.begin(); iter != sell.end(); iter++)
     {
-        executionFile << "order " + sell[i]->orderId + " " << sell[i]->quantity << " shares unexecuted\n";
+        executionFile << "order " + (*iter)->orderId + " " << (*iter)->quantity << " shares unexecuted\n";
     }
-    for (int i = 0; i < buy.size(); i++)
+    for (list<order *>::iterator iter = buy.begin(); iter != buy.end(); iter++)
     {
-        executionFile << "order " + buy[i]->orderId + " " << buy[i]->quantity << " shares unexecuted\n";
+        executionFile << "order " + (*iter)->orderId + " " << (*iter)->quantity << " shares unexecuted\n";
     }
 
     executionFile.close();
--- 6 - Stock_Order/test.cpp
+++ 6 - Stock_Order/test.cpp
@@ -7,26 +7,35 @@ using namespace std;
 int main()
 {
         // std::cout << 34.4232 << std::endl;
-        // cout << std::fixed << std::setprecision(2);
+        cout << std::fixed << std::setprecision(2);
         // std::cout << 34.4232 << std::endl;
         // std::cout << float(10) << std::endl;
 
-    ifstream orderFile;
-    string prevPrice;
-    string prevPrice2;
+    // ifstream orderFile;
+    // string prevPrice;
+    // string prevPrice2;
 
-    orderFile.open("test.txt");
-    orderFile >> prevPrice;
-    cout << prevPrice;
-    int sd;
-    while (orderFile.eof() == 0)
-    {
-        orderFile >> prevPrice >> sd >> prevPrice2;
-        sd++;
-        cout << prevPrice;
-        cout << sd;
-        cout << prevPrice2;
-    }
+    // orderFile.open("test.txt");
+    // orderFile >> prevPrice;
+    // cout << prevPrice;
+    // int sd;
+    // while (orderFile.eof() == 0)
+    // {
+    //     orderFile >> prevPrice >> sd >> prevPrice2;
+    //     sd++;
+    //     cout << prevPrice;
+    //     cout << sd;
+    //     cout << prevPrice2;
+    // }
 
-    return 0;
+        float prevPrice = 143.43;
+
+        cout << "Last trading price: " << prevPrice << endl;
+        cout << "Buy" << setw(30) << "Sell";
+        cout << setfill('-') << setw(50) << endl ;
+        cout << "\norderId "
+             << "price"
+             << " "
+             << "quantity";
+        return 0;
 }
