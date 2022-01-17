796c64cfcbd441ff914c55f587af2f3ec9dde52f
Shrey Jain <shreychanderia@gmail.com>
2022-01-17 16:49:58 +0530

    project 6 completed and project 7 started

--- 6 - Stock_Order/main.cpp
+++ 6 - Stock_Order/main.cpp
@@ -1,11 +1,12 @@
-#include <iomanip>
+#include <iomanip> 
 #include <iostream>
 #include <string>
 #include <list>
 #include <fstream>
 using namespace std;
 
-typedef struct order
+// struct datatype to store required fields
+typedef struct order 
 {
     string orderId;
     float price;
@@ -17,32 +18,32 @@ typedef struct order
 class Stock_order
 {
     float prevPrice;
-    list<order *> *buy;
+    
+    // Using list to reduce time complexity, as we need to insert and delete at random positions
+    list<order *> *buy; 
     list<order *> *sell;
+
     ifstream *orderFile;
     ofstream *executionFile;
 
 public:
     Stock_order(ifstream *orderFile, ofstream *executionFile)
     {
-
+        // initializing our lists
         this->buy = new list<order *>;
         this->sell = new list<order *>;
         this->orderFile = orderFile;
         this->executionFile = executionFile;
     }
 
-    // ~Stock_order()
-    // {
-    //     this->orderFile->close();
-    //     this->executionFile->close();
-    // }
-
+    // function to display all the pending orders
     void display()
     {
+        // Using Manupulators to print data accordingly
+
         cout << "\nLast trading price: " << prevPrice << endl;
-        cout << "Buy" << setw(50) << "Sell" << endl;
-        cout << setfill('-') << setw(75) << "\n";
+        cout << "Buy" << setw(40) << "Sell" << endl; 
+        cout << setfill('-') << setw(36) << "  "<< setw(36) << "\n";
         cout << setfill(' ');
 
         list<order *>::iterator iter1 = buy->begin();
@@ -60,11 +61,11 @@ public:
             }
             else
             {
-                cout << setw(15) << " ";
+                cout << setw(15) << " "; 
             }
             if (iter2 != sell->end())
             {
-                cout << setw(40)
+                cout << setw(30)
                      << (*iter2)->orderId
                      << " "
                      << (*iter2)->price
@@ -79,6 +80,7 @@ public:
         cout << endl;
     }
 
+    // Function to insert data in desending order of their priority
     void insert(order *orderObj)
     {
         char type = orderObj->orderType;
@@ -86,19 +88,15 @@ public:
 
         if (type == 'B')
         {
-
             typename list<order *>::iterator iter = buy->begin();
+         
             if (!buy->empty())
             {
+                // Using iterator to find the correct position for new order, in decreasing priority
                 for (iter = buy->begin(); iter != buy->end(); iter++)
                 {
-                    // cout << "list: " << (*(iter))->orderId << endl;
 
-                    if (type == 'B' && orderObj->price > (*iter)->price)
-                    {
-                        break;
-                    }
-                    else if (type == 'S' && orderObj->price < (*iter)->price)
+                    if (orderObj->price > (*iter)->price)
                     {
                         break;
                     }
@@ -113,15 +111,11 @@ public:
             typename list<order *>::iterator iter = sell->begin();
             if (!sell->empty())
             {
+                // Using iterator to find the correct position for new order, in decreasing priority 
                 for (iter = sell->begin(); iter != sell->end(); iter++)
                 {
-                    // cout << "list: " << (*(iter))->orderId << endl;
-
-                    if (type == 'B' && orderObj->price > (*iter)->price)
-                    {
-                        break;
-                    }
-                    else if (type == 'S' && orderObj->price < (*iter)->price)
+                    
+                    if ( orderObj->price < (*iter)->price)
                     {
                         break;
                     }
@@ -132,6 +126,7 @@ public:
         }
     }
 
+    // Function to check if the current order matches any pending order
     int check(order *vec2)
     {
         char type = vec2->orderType;
@@ -139,15 +134,16 @@ public:
 
         if (type == 'B')
         {
-
             typename list<order *>::iterator iter = sell->begin();
-
+            
+            // Checking all pending sell orders according to their priorities
             for (iter = sell->begin(); iter != sell->end(); iter++)
             {
-
-                if (type == 'B' && (*iter)->price <= vec2->price && (*iter)->quantity == vec2->quantity)
+                // Condition for execution
+                if ((*iter)->price <= vec2->price && (*iter)->quantity == vec2->quantity)
                 {
 
+                    // Writting executed order in execution.txt
                     *(executionFile) << "order " << vec2->orderId << " " << vec2->quantity << " shares purchased at price " << (*iter)->price << "\n";
                     *(executionFile) << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares sold at price " << (*iter)->price << "\n";
 
@@ -155,52 +151,29 @@ public:
                     cout << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares sold at price " << (*iter)->price << "\n";
 
                     prevPrice = (*iter)->price;
+
+                    // Erasing out executed order from list
                     sell->erase(iter);
                     display();
 
                     return 1;
                 }
-                else if (type == 'S' && (*iter)->price >= vec2->price && (*iter)->quantity == vec2->quantity)
-                {
-                    *(executionFile) << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares purchased at price " << (*iter)->price << "\n";
-                    *(executionFile) << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << (*iter)->price << "\n";
-
-                    cout << "\norder " << (*iter)->orderId << " " << (*iter)->quantity << " shares purchased at price " << (*iter)->price << "\n";
-                    cout << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << (*iter)->price << "\n";
-
-                    prevPrice = (*iter)->price;
-                    sell->erase(iter);
-                    display();
-
-                    return 1;
-                }
+                
+                
             }
         }
         else
         {
-
             typename list<order *>::iterator iter = buy->begin();
-
+            
+            // Checking pending buy orders according to their priorities
             for (iter = buy->begin(); iter != buy->end(); iter++)
             {
-
-                if (type == 'B' && (*iter)->price <= vec2->price && (*iter)->quantity == vec2->quantity)
+                // Condition for execution
+                if ((*iter)->price >= vec2->price && (*iter)->quantity == vec2->quantity)
                 {
 
-                    *(executionFile) << "order " << vec2->orderId << " " << vec2->quantity << " shares purchased at price " << (*iter)->price << "\n";
-                    *(executionFile) << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares sold at price " << (*iter)->price << "\n";
-
-                    cout << "\norder " << vec2->orderId << " " << vec2->quantity << " shares purchased at price " << (*iter)->price << "\n";
-                    cout << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares sold at price " << (*iter)->price << "\n";
-
-                    prevPrice = (*iter)->price;
-                    buy->erase(iter);
-                    display();
-
-                    return 1;
-                }
-                else if (type == 'S' && (*iter)->price >= vec2->price && (*iter)->quantity == vec2->quantity)
-                {
+                    // Writting executed order in execution.txt
                     *(executionFile) << "order " << (*iter)->orderId << " " << (*iter)->quantity << " shares purchased at price " << (*iter)->price << "\n";
                     *(executionFile) << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << (*iter)->price << "\n";
 
@@ -208,6 +181,8 @@ public:
                     cout << "order " << vec2->orderId << " " << vec2->quantity << " shares sold at price " << (*iter)->price << "\n";
 
                     prevPrice = (*iter)->price;
+
+                    // Erasing out executed order from list
                     buy->erase(iter);
                     display();
 
@@ -219,28 +194,30 @@ public:
         return 0;
     }
 
+    // Main fucntion to read data from the file
     void driverFunction()
     {
-        // orderFile->open("orders.txt");
-        // executionFile->open("execution.txt");
-
+        // Reading out last transition
         *(orderFile) >> prevPrice;
 
         while ((*(orderFile)).eof() == 0)
         {
             order *orderObj = new order();
 
+            // temporary characters to store limitied and indivisible characters 
             char temp, temp2;
 
+            // Reading a line and storing  in respective variables
             *(orderFile) >> orderObj->orderId >> orderObj->orderType >> temp >> temp2 >> orderObj->price >> orderObj->quantity;
+
             if (orderObj->quantity == 0)
             {
                 break;
             }
-            // cout << orderObj->orderId << orderObj->orderType << temp << temp2 << orderObj->price << orderObj->quantity << endl;
+           
             if (orderObj->orderType == 'B')
             {
-                // cout << "BUY" << endl;
+                // inserting in buy list only if it does not matches any pending orders
                 if (!check(orderObj))
                 {
                     insert(orderObj);
@@ -248,15 +225,15 @@ public:
             }
             else
             {
-                // cout << "Sell" << endl;
+                // inserting in sell list bnly if it does not matches any pending orders
                 if (!check(orderObj))
                 {
                     insert(orderObj);
                 }
             }
-            // display();
         }
 
+        // Writting all unmatched orders in execution file
         for (list<order *>::iterator iter = sell->begin(); iter != sell->end(); iter++)
         {
             (*executionFile) << "order " + (*iter)->orderId + " " << (*iter)->quantity << " shares unexecuted\n";
@@ -270,17 +247,21 @@ public:
 
 int main()
 {
-    cout << std::fixed << std::setprecision(2); // to print upto 2 decimal
+    // to set float digits upto 2 decimal places in console output
+    cout << std::fixed << std::setprecision(2); 
+    std::cout << "Trading begains..." << std::endl ;
 
     ifstream orderFile;
     ofstream executionFile("execution.txt");
     orderFile.open("orders.txt");
 
-    executionFile << std::fixed << std::setprecision(2); // to print upto 2 decimal
+    // to set float digits upto 2 decimal places in execution file
+    executionFile << std::fixed << std::setprecision(2); 
 
     Stock_order stock_order_object(&orderFile, &executionFile);
     stock_order_object.driverFunction();
 
+    // Closing opened files
     executionFile.close();
     orderFile.close();
     return 0;
